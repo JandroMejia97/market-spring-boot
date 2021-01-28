65a15f0dde27c4c746764e1b61323988f7a0bc15
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2021-01-28 10:12:23 -0300

    Se corrige error que causaba que funcionara cualquier usuario.

--- src/main/java/com/jandro/market/domain/service/DefaultUserDetailsService.java
+++ src/main/java/com/jandro/market/domain/service/DefaultUserDetailsService.java
@@ -7,11 +7,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DefaultUserDetailsService implements UserDetailsService {
+    private static List<User> users = new ArrayList<>();
+    public DefaultUserDetailsService() {
+        users.add(new User("admin", "{noop}admin123", new ArrayList<>()));
+    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("admin", "{noop}admin123", new ArrayList<>());
+        Optional<User> user = users.stream().filter(u -> u.getUsername().equals(username)).findAny();
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found by name: " + username);
+        }
+        return user.get();
     }
 }
